7d135d4969e3fc9ad10f5522b3b24b4ff283da7d
yakobodata <jacobwamanidata@gmail.com>
2026-09-12 18:10:23 +0300

    Step11

---
+++ lib/services/crud/crud_exceptions.dart
@@ -0,0 +1,16 @@
+
+class DatabaseAlreadyOpenException implements Exception{}
+
+class UnableToGetDocumentsDirectory implements Exception{}
+
+class DatabaseIsNotOpen implements Exception{}
+
+class CouldNotDeleteUser implements Exception{}
+
+class CouldNotFindUser implements Exception{}
+
+class UserAlreadyExists implements Exception{}
+
+class CouldNotFindNote implements Exception{}
+
+class CouldNoteUpdateNote implements Exception{}
